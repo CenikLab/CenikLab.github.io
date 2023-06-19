5bef5729a3231072dc71453207998a47c952eec1
Jonathan Chacko <jonathan.j.chacko@gmail.com>
2023-06-19 12:38:19 -0500

    attempt to fix masonry bug again

--- _javascript/modules/components/masonry.js
+++ _javascript/modules/components/masonry.js
@@ -1,17 +1,18 @@
 export function loadGrid() {
-  const gridElement = $('.masonry-grid');
-  if (gridElement.length) {
-    const grid = $('.masonry-grid').masonry({
-      percentPosition: true
-    });
+  $(() => {
+    const gridElement = $('.masonry-grid');
+    if (gridElement.length) {
+      const grid = $('.masonry-grid').masonry({
+        percentPosition: true
+      });
 
-    // layout Masonry after each image loads
-    grid.imagesLoaded().progress(function () {
+      // layout Masonry after each image loads
+      grid.imagesLoaded().progress(function () {
+        grid.masonry('layout');
+      });
+
+      // layout masonry on init
       grid.masonry('layout');
-    });
-
-    // layout masonry on init
-    grid.masonry('layout');
-    $(() => grid.masonry('layout'))
-  }
+    }
+  });
 }
